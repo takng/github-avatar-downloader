cf8eaaae5173ae7c5bfac449bbba68d88dde5c32
TakNg <tak_ng@yahoo.com>
2017-07-04 04:43:31 +0000

    avatar

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,6 @@ var request = require('request');
 
 var GITHUB_USER = "takng";
 var GITHUB_TOKEN = "4c027e80b056b8dbdd22055d8c1a662b70a25cfe";
-var API_KEY = "takng:4c027e80b056b8dbdd22055d8c1a662b70a25cfe";
-var query = "jer";
 var json;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -11,39 +9,27 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
 
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-var url_obj = {
+  var url_obj = {
     url: requestURL,
     headers: {
       'User-Agent': 'GitHub Avatar Downloader - Student Project'
     },
   };
 
-// var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-
-//  var requestURL = `https://api.github.com/repos/jquery/jquery/contributors/search?key=${API_KEY}&q=${query}`;
-
 console.log(requestURL);
 
-//https://api.github.com/repos/jquery/jquery/contributors
-
-  //request(requestURL, function(err, response, body) {
   request(url_obj, function(err, response, body) {
     if (err) {
       console.log(`Error fetching: ${requestURL}`, err);
       return;
     }
 
-console.log(response.statusCode);
-// json = JSON.parse(body);
-// console.log(json.login);
+//  console.log(response.statusCode);
 
     if (response.statusCode === 200) {
-console.log(body);
       json = JSON.parse(body);
-//console.log(json.avatar_url);
       cb(0, json);
     }
   });
@@ -51,22 +37,17 @@ console.log(body);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  // console.log("Result:", result);
+  // console.log("Errors:", err);
 
-var i = 0;
-for (key in result) {  
-if (result.hasOwnProperty(key)) {  
+  for (key in result) {  
+  if (result.hasOwnProperty(key)) {  
     var obj = result[key];
-    console.log(key);
-    //console.log(obj);
+    // console.log(key);
     console.log(obj.avatar_url);
-  console.log('-----');
-}
+    // console.log('-----');
+  }
 }
 
-
-    
 });
 
 
